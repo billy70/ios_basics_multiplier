d37820afa566a174f99afb5669cd1b2242262e9c
William L. Marr III <hobbyist@wlmiii.net>
2016-03-02 15:30:59 -0500

    Add outlets for controls

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var instructionsLabel: UILabel!
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
